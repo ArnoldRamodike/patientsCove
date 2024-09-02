e9aaaad938751e7a7b0d6e3e1c2f18be86144a1e
ArnoldRamodike <riccoarnold23@gmail.com>
2024-09-02 11:36:48 +0200

    Project initializations

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex ">
+      <h1 className="text-white">Home</h1>
+    </div>
+  );
+}
