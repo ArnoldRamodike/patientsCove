c223d42d0b9c0736ff4e8503fd7fba21be5579c3
ArnoldRamodike <riccoarnold23@gmail.com>
2024-09-10 10:34:15 +0200

    New appointment page

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,9 +1,42 @@
+import AppintmentForm from "@/components/forms/AppintmentForm";
+import { getPatient } from "@/lib/actions/actions";
+import Image from "next/image";
 import React from "react";
 
-const NewAppointment = () => {
+const NewAppointment = async ({ params: { userId } }: SearchParamProps) => {
+  const patient = await getPatient(userId);
+
+  // console.log("patient: ", patient);
+
   return (
-    <div>
-      <h1>Appointment</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src={"/assets/icons/logo-full.svg"}
+            alt="Patient"
+            height={1000}
+            width={1000}
+            className="mb-12 h-10 w-fit"
+          />
+
+          <AppintmentForm
+            type="create"
+            userId={userId}
+            patientId={patient.$id}
+          />
+
+          <p className="copyright mt-10 py-12">@copy; 2024 CarePulse</p>
+        </div>
+      </section>
+
+      <Image
+        src={"/assets/images/appointment-img.png"}
+        height={1000}
+        width={1000}
+        alt="Patient"
+        className="side-img max-w-[390px] bg-bottom"
+      />
     </div>
   );
 };
