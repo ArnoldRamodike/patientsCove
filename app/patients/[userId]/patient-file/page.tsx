47400676eee6a14cc73f233e6f0ef48ac19755cb
ArnoldRamodike <riccoarnold23@gmail.com>
2024-12-03 22:56:14 +0200

    Justify the init of the patien file

--- app/patients/[userId]/patient-file/page.tsx
+++ app/patients/[userId]/patient-file/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const PatientFile = () => {
-  return <div>PatientFile</div>;
+  return (
+    <div className="flex h-screen max-h-screen">
+      <h1>Patient files</h1>
+    </div>
+  );
 };
 
 export default PatientFile;
