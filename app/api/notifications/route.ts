c59b63fd7afdbb0bce2cb5e165cbc683d2bdeed4
ArnoldRamodike <riccoarnold23@gmail.com>
2024-12-07 23:36:18 +0200

    Some few updates

---
+++ app/api/notifications/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse, NextRequest } from "next/server";
+
+
+// A faulty API route to test Sentry's error monitoring
+export function GET() {
+ 
+}
--- app/patients/page.tsx
+++ app/patients/page.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const Patients = () => {
   return (
-    <div>
-      <h1>Patient List</h1>
+    <div className="flex h-screen max-h-screen">
+      <h1 className="text-2xl text-red-500">Patient List</h1>
+
+      <div className="">
+        <p>Patients lists here</p>
+      </div>
     </div>
   );
 };
