6d349f9f54d99688bea4081cc093245c407e0e74
ArnoldRamodike <riccoarnold23@gmail.com>
2025-03-21 21:40:26 +0200

    New patient login form init

--- app/patients/[userId]/patient-file/page.tsx
+++ app/patients/[userId]/patient-file/page.tsx
@@ -4,6 +4,7 @@ const PatientFile = () => {
   return (
     <div className="flex h-screen max-h-screen">
       <h1>Patient files</h1>
+      <div className=""></div>
     </div>
   );
 };
---
+++ components/forms/LoginForm.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form, FormControl } from "@/components/ui/form";
+import { Label } from "@/components/ui/label";
+import { PatientFormDefaultValues } from "@/constants";
+import { registerPatient } from "@/lib/actions/actions";
+import { PatientFormValidation } from "@/lib/validation";
+
+// import { FileUploader } from "../FileUploader";
+import SubmitButton from "../SubmitButton";
+import CustomFormField from "../CustomFormField";
+import { FormFieldType } from "./PatientForm";
+
+const RegisterForm = ({ user }: { user: User }) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof PatientFormValidation>>({
+    resolver: zodResolver(PatientFormValidation),
+    defaultValues: {
+      ...PatientFormDefaultValues,
+      name: user.name,
+      email: user.email,
+      phone: user.phone,
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof PatientFormValidation>) => {
+    setIsLoading(true);
+
+    // Store file info in form data as
+    let formData;
+    if (
+      values.identificationDocument &&
+      values.identificationDocument?.length > 0
+    ) {
+      const blobFile = new Blob([values.identificationDocument[0]], {
+        type: values.identificationDocument[0].type,
+      });
+
+      formData = new FormData();
+      formData.append("blobFile", blobFile);
+      formData.append("fileName", values.identificationDocument[0].name);
+    }
+
+    try {
+      const patient = {
+        userId: user.$id,
+        name: values.name,
+        email: values.email,
+      };
+
+      const existingPatient = await registerPatient(patient);
+
+      if (existingPatient) {
+        router.push(`/patients/${user.$id}/new-appointment`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex-1 space-y-12"
+      >
+        <section className="space-y-4">
+          <h1 className="header">Welcome Back 👋</h1>
+          <p className="text-dark-700">
+            Please login to view your Carepulse Information.
+          </p>
+        </section>
+
+        <section className="space-y-6">
+          <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Login</h2>
+          </div>
+
+          {/* EMAIL */}
+          <div className="flex flex-col gap-6 xl:flex-row">
+            <CustomFormField
+              fieldType={FormFieldType.INPUT}
+              control={form.control}
+              name="email"
+              label="Email address"
+              placeholder="johndoe@gmail.com"
+              iconSrc="/assets/icons/email.svg"
+              iconAlt="email"
+            />
+          </div>
+
+          {/* NAME */}
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="Password"
+            placeholder="Pa$$w0rd"
+            iconSrc="/assets/icons/user.svg"
+            iconAlt="user"
+          />
+        </section>
+
+        <SubmitButton isLoading={isLoading}>Login</SubmitButton>
+      </form>
+    </Form>
+  );
+};
+
+export default RegisterForm;
